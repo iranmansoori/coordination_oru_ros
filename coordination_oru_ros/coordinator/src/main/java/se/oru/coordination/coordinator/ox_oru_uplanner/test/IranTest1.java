75d858da036bfa22b2fac606e9b2324b65885ba8
Iran <masoumeh.mansouri@oru.se>
2018-06-21 11:40:36 +0100

    add a new test

---
+++ coordination_oru_ros/coordinator/src/main/java/se/oru/coordination/coordinator/ox_oru_uplanner/test/IranTest1.java
@@ -0,0 +1,17 @@
+package se.oru.coordination.coordinator.ox_oru_uplanner.test;
+
+
+import se.oru.coordination.coordinator.ros_coordinator.TrajectoryEnvelopeCoordinatorROS;
+
+
+public class IranTest1 {
+
+	private TrajectoryEnvelopeCoordinatorROS tec = null;	
+	public static void main(String[] args) {
+		System.out.println();
+		
+
+
+	}
+
+}
